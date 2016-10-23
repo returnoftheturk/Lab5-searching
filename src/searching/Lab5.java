2a923c56e4b48efd1f578817895d9db5bead81c3
Steve Lee <seung.jin.lee@mail.mcgill.ca>
2016-10-22 22:37:14 -0400

    fixed error
    used navigator instead of navigation

--- src/searching/Lab5.java
+++ src/searching/Lab5.java
@@ -23,6 +23,7 @@ public class Lab5 {
 	public static final double WHEEL_RADIUS = 2.1;
 	public static final double TRACK = 14.6;
 	private static Navigation nav;
+	private static Navigator navr;
 
 	public static void main(String[] args) {
 
@@ -110,6 +111,7 @@ public class Lab5 {
 			t.clear();
 			UltrasonicPoller usPoller = new UltrasonicPoller(usValue, usData, usl);
 			LCDInfo lcd = new LCDInfo(odo, usl, lsl, LCDInfo.DemoType.OBJECT_SEARCH_FIND, blockDetection);
+			navr = new Navigator(odo,usPoller);
 			usPoller.start();
 			usl.doLocalization();
 			// odometer.start();
@@ -134,8 +136,8 @@ public class Lab5 {
 		int[][] waypoints = { { 60, 30 }, { 30, 30 }, { 30, 60 }, { 60, 0 } };
 
 		for (int[] point : waypoints) {
-			nav.travelTo(point[0], point[1], true);
-			while (nav.isTravelling()) {
+			navr.travelTo(point[0], point[1], true);
+			while (navr.isTravelling()) {
 				try {
 					Thread.sleep(500);
 				} catch (InterruptedException e) {
